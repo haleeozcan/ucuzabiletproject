3e6371aef5e91c190830bdbbbd72db89e1b080a8
haleeozcan <haleozcan17@hotmail.com>
2021-10-31 16:19:41 +0300

    Navigation added

--- src/App.js
+++ src/App.js
@@ -2,16 +2,36 @@ import './App.css';
 import Header from "./components/Header";
 import OdaSecenekleri from "./components/OdaSecenekleri";
 import OdaBilgileri from "./components/OdaBilgileri";
+import Tabs from "react-bootstrap/Tabs";
+import Tab from 'react-bootstrap/Tab'
 
 function App() {
-  return (
-    <div className="App">
-      <Header className="App-header">
-      </Header>
-        <OdaSecenekleri/>
-        <OdaBilgileri/>
-    </div>
-  );
+    return (
+        <div className="App">
+            <Header className="App-header">
+            </Header>
+            <Tabs defaultActiveKey="odasecenekleri" id="uncontrolled-tab-example" className="mb-3"   transition={false}
+            >
+                <Tab eventKey="odasecenekleri" title="Oda Seçenekleri">
+                    <OdaSecenekleri/>
+                    <OdaBilgileri/>
+                </Tab>
+                <Tab eventKey="genelbilgi" title="Genel Bilgi">
+                    Genel Bilgi
+                </Tab>
+                <Tab eventKey="tesisolanaklari" title="Tesis Olanakları">
+                    Tesis Olanakları
+                </Tab>
+                <Tab eventKey="detaylibilgi" title="Detaylı Bilgi">
+                    Detaylı Bilgi
+                </Tab>
+                <Tab eventKey="yorumlar" title="Yorumlar">
+                    Yorumlar
+                </Tab>
+            </Tabs>
+
+        </div>
+    );
 }
 
 export default App;
